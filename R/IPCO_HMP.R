ff27cc4d2a71c7329cdce38a6e8da34e71fbcd0c
IPCO-Rlibrary <IPCO.Rlibrary@gmail.com>
2019-06-16 16:35:22 +0100

    Uploads

---
+++ R/IPCO_HMP.R
@@ -0,0 +1,12 @@
+#' HMP data
+#'
+#' @docType data
+#'
+#' @usage load_ref(samples="HMP")
+#'
+#' @format An object of class list
+#'
+#' @keywords datasets
+#' @examples
+#' load_ref(samples="HMP")
+"IPCO_HMP"
---
+++ R/IPCO_Healthy.R
@@ -0,0 +1,12 @@
+#' Healthy cohort
+#'
+#' @docType data
+#'
+#' @usage load_ref(IPCO_Healthy)
+#'
+#' @format An object of class list
+#'
+#' @keywords datasets
+#' @examples
+#' load_ref()
+"IPCO_Healthy"
