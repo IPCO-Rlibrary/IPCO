65ce8816c145777d8b4442dc6fe744b1d71c89d7
IPCO-Rlibrary <IPCO.Rlibrary@gmail.com>
2019-06-17 10:52:59 +0100

    Uploads

--- R/filter_functionality.R
+++ R/filter_functionality.R
@@ -5,7 +5,7 @@
 #' You can opt to not filter the dataset at all so that all functionality is predicted.
 #' @param Requires the dataset to be transformed and whether to filter by covearge or abundance.
 #' @param dataset  dataset where rows are functions/pathways and columns are samples.
-#' @param type the type of dataset provided: (coverage or abundance).
+#' @param type the type of dataset provided: (KEGG or MetaCyc).
 #' @param Optional threshold_dataset, levels.
 #' @param threshold_dataset dataset containing the information about pathway/function coverage. Threshold dataset should contain the same functions/pathways as present in dataset.
 #' @param levels Default is 0.01 for coverge or the first quartile of the mean function/pathway abundance.
@@ -17,7 +17,7 @@
 filter_functionality <- function(dataset,type,threshold_dataset,threshold){
 
 
-if (type == "coverage"){
+if (type == "kegg"){
 						if(missing(threshold_dataset)){
 						print("Please provide coverage dataset")
 						}else{
@@ -32,7 +32,7 @@ if (type == "coverage"){
 								  return(filtered_dataset)
 								  }
 								  }
-} else  if (type == "abundance"){
+} else  if (type == "metacyc"){
 								if (missing(threshold)){
 								print("threshold not provided, removing pathways with values less than 1st quartile of the mean pathway abundance")
 								filtered_dataset <- dataset[names(which(rowMeans(dataset) >= summary(rowMeans(dataset))[2])),]
@@ -43,7 +43,7 @@ if (type == "coverage"){
 
 									 }
 								} else {
-										print("error in type: Please enter coverage or abundance in type")
+										print("error in type: Please enter type=\"kegg\" or type=\"metacyc\"")
 										}
 }
 
